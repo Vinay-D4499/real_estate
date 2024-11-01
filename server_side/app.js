e0811d73ecdca5ce5fe02755b81613ab660ab41e
Vinay D <vinayhari789@gmail.com>
2024-11-01 17:08:06 +0530

    webhook config

--- server_side/app.js
+++ server_side/app.js
@@ -81,36 +81,80 @@ app.get("/webhook",(req,res)=>{
  
  });
  
- app.post('/webhook', (req, res) => {
-    const bodyParam = req.body;
+//  app.post('/webhook', (req, res) => {
+//     const bodyParam = req.body;
 
-    // Check for incoming messages
-    if (bodyParam.object === 'whatsapp_business_account') {
-        const changes = bodyParam.entry[0].changes;
-        if (changes && changes[0].value.messages && changes[0].value.messages[0]) {
-            const messageData = changes[0].value;
-            const from = messageData.messages[0].from; // Sender's WhatsApp number
-            const msgBody = messageData.messages[0].text.body; // Text content of the message
+//     // Check for incoming messages
+//     if (bodyParam.object === 'whatsapp_business_account') {
+//         const changes = bodyParam.entry[0].changes;
+//         if (changes && changes[0].value.messages && changes[0].value.messages[0]) {
+//           let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//                let from = body_param.entry[0].changes[0].value.messages[0].from; 
+//                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-            // Log the message details
-            console.log("New message received:");
-            console.log("From:", from);
-            console.log("Message Body:", msgBody);
+//             // Log the message details
+//             console.log("New message received:");
+//             console.log("From:", from);
+//             console.log("Message Body:", msgBody);
 
-            // Example: Store the response in a database or further process it
-            // db.saveMessage({ from, body: msgBody });
+//             // Example: Store the response in a database or further process it
+//             // db.saveMessage({ from, body: msgBody });
 
-            // Respond to WhatsApp to acknowledge message receipt
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404); // No message found in the payload
-        }
-    } else {
-        res.sendStatus(404); // Invalid webhook event
+//             // Respond to WhatsApp to acknowledge message receipt
+//             res.sendStatus(200);
+//         } else {
+//             res.sendStatus(404); // No message found in the payload
+//         }
+//     } else {
+//         res.sendStatus(404); // Invalid webhook event
+//     }
+// });
+
+app.post("/webhook",(req,res)=>{ //i want some 
+
+    let body_param=req.body;
+
+    console.log(JSON.stringify(body_param,null,2));
+
+    if(body_param.object){
+        console.log("inside body param");
+        if(body_param.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
+
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
+               res.sendStatus(200);
+            }else{
+                res.sendStatus(404);
+            }
+
     }
+
 });
-
-
 
 async function sendTextMessage(to, phone, email, password) {
     try {
