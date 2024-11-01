c710283438c7a499b056a25eeb381f19fdf30986
Vinay D <vinayhari789@gmail.com>
2024-11-01 15:07:25 +0530

    webhook call backe url and token for api verification

--- server_side/app.js
+++ server_side/app.js
@@ -14,7 +14,7 @@ const propertyRoutes = require('./routes/propertyRoutes');
 const { baseURL } = require('./config/baseURL');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Middleware setup
 app.use(cors({
@@ -59,3 +59,70 @@ initDatabase().then(async () => {
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+const token=process.env.WHATSAPP_TOKEN;
+const mytoken=process.env.CHECK_TOKEN;
+
+app.get("/webhook",(req,res)=>{
+    let mode=req.query["hub.mode"];
+    let challange=req.query["hub.challenge"];
+    let token=req.query["hub.verify_token"];
+ 
+ 
+     if(mode && token){
+ 
+         if(mode==="subscribe" && token===mytoken){
+             res.status(200).send(challange);
+         }else{
+             res.status(403);
+         }
+ 
+     }
+ 
+ });
+ 
+ app.post("/webhook",(req,res)=>{ 
+ 
+     let body_param=req.body;
+ 
+     console.log(JSON.stringify(body_param,null,2));
+ 
+     if(body_param.object){
+         console.log("inside body param");
+         if(body_param.entry && 
+             body_param.entry[0].changes && 
+             body_param.entry[0].changes[0].value.messages && 
+             body_param.entry[0].changes[0].value.messages[0]  
+             ){
+                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+                let from = body_param.entry[0].changes[0].value.messages[0].from; 
+                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+ 
+                console.log("phone number "+phon_no_id);
+                console.log("from "+from);
+                console.log("boady param "+msg_body);
+ 
+                axios({
+                    method:"POST",
+                    url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                    data:{
+                        messaging_product:"whatsapp",
+                        to:from,
+                        text:{
+                            body:"Hi.. I'm Prasath, your message is "+msg_body
+                        }
+                    },
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+ 
+                });
+ 
+                res.sendStatus(200);
+             }else{
+                 res.sendStatus(404);
+             }
+ 
+     }
+ 
+ });
